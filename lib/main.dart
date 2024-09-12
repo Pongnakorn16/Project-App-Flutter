0cefa7911521dea98e68003937c7f0fdbc768810
DOK <148057045+Pongnakorn16@users.noreply.github.com>
2024-09-13 06:45:29 +0700

    FIX reset system Delete cart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:my_first_app/pages/login.dart';
 import 'package:mobile_miniproject_app/pages/Login.dart';
 import 'package:mobile_miniproject_app/pages/Splash.dart';
 import 'package:mobile_miniproject_app/shared/share_data.dart';
